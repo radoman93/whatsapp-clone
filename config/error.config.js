49b73eb9a8407875dd55362fc8fd5e44227501b0
unknown <radomanp@gmail.com>
2022-05-04 13:30:08 +0200

    config folder

---
+++ config/error.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  ERR_USER_REGISTER: 'User register failed',
+  ERR_USER_NOT_FOUND: 'User not found',
+  ERR_VALIDATION: 'Validation error',
+  ERR_SERVER_ERROR: 'Server error',
+  ERR_PHONE_ALREADY_REGISTERED: 'Phone is already registered!',
+  ERR_CONVERSATION_NOT_FOUND: 'Conversation not found',
+  ERR_MESSAGE_NOT_FOUND: 'Message not found',
+}
